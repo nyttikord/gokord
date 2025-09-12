9f9ec54f8aadc52fef4e373607038cf76704e960
William Hergès <william@herges.fr>
2025-09-12 15:34:06 +0200

    refactor(restapi): move premium related in its own new subpackage

--- restapi.go
+++ restapi.go
@@ -4,15 +4,11 @@ import (
 	_ "image/jpeg" // For JPEG decoding
 	_ "image/png"  // For PNG decoding
 	"net/http"
-	"net/url"
-	"strconv"
 	"strings"
-	"time"
 
 	"github.com/nyttikord/gokord/application"
 	"github.com/nyttikord/gokord/channel"
 	"github.com/nyttikord/gokord/discord"
-	"github.com/nyttikord/gokord/premium"
 	"github.com/nyttikord/gokord/user"
 )
 
@@ -82,10 +78,6 @@ func (s *Session) GatewayBot(options ...discord.RequestOption) (*GatewayBotRespo
 
 	return &resp, nil
 }
-
-// ----------------------------------------------------------------------
-// Functions specific to auto moderation
-// ----------------------------------------------------------------------
 
 // ApplicationRoleConnectionMetadata returns application role connection metadata.
 // appID : ID of the application
@@ -189,139 +181,5 @@ func (s *Session) PollExpire(channelID, messageID string) (msg *channel.Message,
 	}
 
 	err = unmarshal(body, &msg)
-	return
-}
-
-// ----------------------------------------------------------------------
-// Functions specific to monetization
-// ----------------------------------------------------------------------
-
-// SKUs returns all SKUs for a given application.
-// appID : The ID of the application.
-func (s *Session) SKUs(appID string) (skus []*premium.SKU, err error) {
-	endpoint := discord.EndpointApplicationSKUs(appID)
-
-	body, err := s.RequestWithBucketID("GET", endpoint, nil, endpoint)
-	if err != nil {
-		return
-	}
-
-	err = unmarshal(body, &skus)
-	return
-}
-
-// Entitlements returns all Entitlements for a given app, active and expired.
-// appID			: The ID of the application.
-// filterOptions	: Optional filter options; otherwise set it to nil.
-func (s *Session) Entitlements(appID string, filterOptions *premium.EntitlementFilterOptions, options ...discord.RequestOption) (entitlements []*premium.Entitlement, err error) {
-	endpoint := discord.EndpointEntitlements(appID)
-
-	queryParams := url.Values{}
-	if filterOptions != nil {
-		if filterOptions.UserID != "" {
-			queryParams.Set("user_id", filterOptions.UserID)
-		}
-		if filterOptions.SkuIDs != nil && len(filterOptions.SkuIDs) > 0 {
-			queryParams.Set("sku_ids", strings.Join(filterOptions.SkuIDs, ","))
-		}
-		if filterOptions.Before != nil {
-			queryParams.Set("before", filterOptions.Before.Format(time.RFC3339))
-		}
-		if filterOptions.After != nil {
-			queryParams.Set("after", filterOptions.After.Format(time.RFC3339))
-		}
-		if filterOptions.Limit > 0 {
-			queryParams.Set("limit", strconv.Itoa(filterOptions.Limit))
-		}
-		if filterOptions.GuildID != "" {
-			queryParams.Set("guild_id", filterOptions.GuildID)
-		}
-		if filterOptions.ExcludeEnded {
-			queryParams.Set("exclude_ended", "true")
-		}
-	}
-
-	body, err := s.RequestWithBucketID("GET", endpoint+"?"+queryParams.Encode(), nil, endpoint, options...)
-	if err != nil {
-		return
-	}
-
-	err = unmarshal(body, &entitlements)
-	return
-}
-
-// EntitlementConsume marks a given One-Time Purchase for the user as consumed.
-func (s *Session) EntitlementConsume(appID, entitlementID string, options ...discord.RequestOption) (err error) {
-	_, err = s.RequestWithBucketID("POST", discord.EndpointEntitlementConsume(appID, entitlementID), nil, discord.EndpointEntitlementConsume(appID, ""), options...)
-	return
-}
-
-// EntitlementTestCreate creates a test entitlement to a given SKU for a given guild or user.
-// Discord will act as though that user or guild has entitlement to your premium offering.
-func (s *Session) EntitlementTestCreate(appID string, data *premium.EntitlementTest, options ...discord.RequestOption) (err error) {
-	endpoint := discord.EndpointEntitlements(appID)
-
-	_, err = s.RequestWithBucketID("POST", endpoint, data, endpoint, options...)
-	return
-}
-
-// EntitlementTestDelete deletes a currently-active test entitlement. Discord will act as though
-// that user or guild no longer has entitlement to your premium offering.
-func (s *Session) EntitlementTestDelete(appID, entitlementID string, options ...discord.RequestOption) (err error) {
-	_, err = s.RequestWithBucketID("DELETE", discord.EndpointEntitlement(appID, entitlementID), nil, discord.EndpointEntitlement(appID, ""), options...)
-	return
-}
-
-// Subscriptions returns all subscriptions containing the SKU.
-// skuID : The ID of the SKU.
-// userID : User ID for which to return subscriptions. Required except for OAuth queries.
-// before : Optional timestamp to retrieve subscriptions before this time.
-// after : Optional timestamp to retrieve subscriptions after this time.
-// limit : Optional maximum number of subscriptions to return (1-100, default 50).
-func (s *Session) Subscriptions(skuID string, userID string, before, after *time.Time, limit int, options ...discord.RequestOption) (subscriptions []*premium.Subscription, err error) {
-	endpoint := discord.EndpointSubscriptions(skuID)
-
-	queryParams := url.Values{}
-	if before != nil {
-		queryParams.Set("before", before.Format(time.RFC3339))
-	}
-	if after != nil {
-		queryParams.Set("after", after.Format(time.RFC3339))
-	}
-	if userID != "" {
-		queryParams.Set("user_id", userID)
-	}
-	if limit > 0 {
-		queryParams.Set("limit", strconv.Itoa(limit))
-	}
-
-	body, err := s.RequestWithBucketID("GET", endpoint+"?"+queryParams.Encode(), nil, endpoint, options...)
-	if err != nil {
-		return
-	}
-
-	err = unmarshal(body, &subscriptions)
-	return
-}
-
-// Subscription returns a subscription by its SKU and subscription ID.
-// skuID : The ID of the SKU.
-// subscriptionID : The ID of the subscription.
-// userID : User ID for which to return the subscription. Required except for OAuth queries.
-func (s *Session) Subscription(skuID, subscriptionID, userID string, options ...discord.RequestOption) (subscription *premium.Subscription, err error) {
-	endpoint := discord.EndpointSubscription(skuID, subscriptionID)
-
-	queryParams := url.Values{}
-	if userID != "" {
-		// Unlike stated in the documentation, the user_id parameter is required here.
-		queryParams.Set("user_id", userID)
-	}
-
-	body, err := s.RequestWithBucketID("GET", endpoint+"?"+queryParams.Encode(), nil, endpoint, options...)
-	if err != nil {
-		return
-	}
-
-	err = unmarshal(body, &subscription)
 	return
 }
