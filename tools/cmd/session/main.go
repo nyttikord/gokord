9b8f305dc1955adc0a65816b0eefba7774795a03
William Hergès <william@herges.fr>
2025-10-19 11:53:49 +0200

    feat(tools): cmd to run a simple session
    use go run tools/cmd/session/main.go to start a new simple session to test the session, supports env var DG_BOT and timeout with -timeout

---
+++ tools/cmd/session/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/nyttikord/gokord"
+	"github.com/nyttikord/gokord/bot"
+	"github.com/nyttikord/gokord/event"
+)
+
+var (
+	token   string
+	timeout int
+)
+
+func init() {
+	flag.StringVar(&token, "token", os.Getenv("DG_TOKEN"), "token of the bot (required)")
+	flag.IntVar(&timeout, "timeout", 0, "timeout in minutes")
+}
+
+func main() {
+	flag.Parse()
+	if token == "" {
+		println("token not set, use '-token' to set it")
+		return
+	}
+	var ctx context.Context
+	if timeout <= 0 {
+		ctx = context.Background()
+	} else {
+		var cancel func()
+		ctx, cancel = context.WithTimeout(context.Background(), time.Duration(timeout)*time.Minute)
+		defer cancel()
+	}
+	dg := gokord.NewWithLogLevel("Bot "+token, slog.LevelDebug)
+	dg.EventManager().AddHandler(func(_ context.Context, s bot.Session, r *event.Ready) {
+		s.Logger().Info("bot ready")
+	})
+	err := dg.OpenAndBlock(ctx)
+	if err != nil && !errors.Is(err, ctx.Err()) {
+		panic(err)
+	}
+}
